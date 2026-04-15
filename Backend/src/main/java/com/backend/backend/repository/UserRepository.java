e8b2afef94bad081fa78c02039744afd593f3255
darky9197 <darkking9197@gmail.com>
2026-04-15 10:04:35 +0530

    intial commit

---
+++ Backend/src/main/java/com/backend/backend/repository/UserRepository.java
@@ -0,0 +1,11 @@
+package com.backend.backend.repository;
+
+import com.backend.backend.model.Users;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+import java.util.UUID;
+
+public interface UserRepository extends JpaRepository<Users, UUID> {
+    public Optional<Users> findByEmail(String email);
+}
