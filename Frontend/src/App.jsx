ba1f914fffa35e2aae4b144a6c4c1e16f7fd672a
darky9197 <darkking9197@gmail.com>
2026-04-15 10:13:23 +0530

    frontend

---
+++ Frontend/src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import { Button } from './components/ui/button'
+
+function App() {
+
+  return (
+    <>
+      <Button className="p-8 m-4 text-5xl" variant='outline'>Hello</Button>
+      <Button className="p-8 m-4 text-5xl" variant='outline'>Hello</Button>
+    </>
+  )
+}
+
+export default App
